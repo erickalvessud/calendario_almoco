c557513b191537317a469cdaab0485eb2284d6cc
Erick <ealves@magnasistemas.com.br>
2016-12-06 16:53:27 -0200

    Working on lunch calendar page and business rules.

--- calendarioalmoco-web/src/main/java/com/erick/calendarioalmoco/bean/LunchCalendarMB.java
+++ calendarioalmoco-web/src/main/java/com/erick/calendarioalmoco/bean/LunchCalendarMB.java
@@ -4,8 +4,11 @@ import java.io.Serializable;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
+import java.util.TimeZone;
 
 import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
 import javax.inject.Named;
 
 import org.primefaces.event.SelectEvent;
@@ -14,20 +17,55 @@ import org.primefaces.model.DefaultScheduleModel;
 import org.primefaces.model.ScheduleEvent;
 import org.primefaces.model.ScheduleModel;
 
+import com.erick.calendarioalmoco.dao.ChurchMemberDAO;
+import com.erick.calendarioalmoco.modelo.ChurchMember;
+
 @Named
 @ViewScoped
 public class LunchCalendarMB implements Serializable{
 
+	@Inject
+	private ChurchMemberDAO churchMemberDAO;
+	
 	private ScheduleModel eventModel = new DefaultScheduleModel();
+	private ScheduleEvent event = new DefaultScheduleEvent();
+	
+	private List<ChurchMember> churchMembers;
+	
+	private ChurchMember churchMemberSelected;
 
+	public LunchCalendarMB(){
+		this.churchMemberSelected = new ChurchMember();
+	}
 	
 	public void onDateSelect(SelectEvent selectEvent){
 		Date dateSelected = (Date)selectEvent.getObject();
-		Calendar calendar = GregorianCalendar.getInstance();
+		
+		this.event = new DefaultScheduleEvent("", (Date) selectEvent.getObject(), (Date) selectEvent.getObject());
+		
+		Calendar calendar = Calendar.getInstance();
 		calendar.setTime(dateSelected);
 		int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
-		System.out.println("Dia da semana: "+dayOfWeek);
+		this.churchMembers = this.churchMemberDAO.findByAvailableWeekDay(dayOfWeek);
 	}
+	
+	public void onRowDblClckSelect(final SelectEvent event) {
+		this.churchMemberSelected = (ChurchMember) event.getObject();
+		this.addAppointment();
+	}
+	
+	private void addAppointment(){
+		
+		((DefaultScheduleEvent) this.event).setTitle(churchMemberSelected.getName());
+		
+        if(this.event.getId() == null){
+            this.eventModel.addEvent(this.event);
+        }else{
+        	this.eventModel.updateEvent(this.event);
+		}
+        this.event = new DefaultScheduleEvent();
+	}
+	
 	/**
 	 * @return the eventModel
 	 */
@@ -42,5 +80,30 @@ public class LunchCalendarMB implements Serializable{
 		this.eventModel = eventModel;
 	}
 
+	/**
+	 * @return the churchMembers
+	 */
+	public List<ChurchMember> getChurchMembers() {
+		return churchMembers;
+	}
+	/**
+	 * @param churchMembers the churchMembers to set
+	 */
+	public void setChurchMembers(List<ChurchMember> churchMembers) {
+		this.churchMembers = churchMembers;
+	}
+	
+	/**
+	 * @return the churchMemberSelected
+	 */
+	public ChurchMember getChurchMemberSelected() {
+		return churchMemberSelected;
+	}
 
+	/**
+	 * @param churchMemberSelected the churchMemberSelected to set
+	 */
+	public void setChurchMemberSelected(ChurchMember churchMemberSelected) {
+		this.churchMemberSelected = churchMemberSelected;
+	}
 }
--- calendarioalmoco-web/src/main/java/com/erick/calendarioalmoco/dao/ChurchMemberDAO.java
+++ calendarioalmoco-web/src/main/java/com/erick/calendarioalmoco/dao/ChurchMemberDAO.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import org.slf4j.Logger;
 
+import com.erick.calendarioalmoco.common.DaysOfTheWeekEnum;
 import com.erick.calendarioalmoco.modelo.ChurchMember;
 
 public class ChurchMemberDAO extends GenericDAO<ChurchMember>{
@@ -27,6 +29,36 @@ public class ChurchMemberDAO extends GenericDAO<ChurchMember>{
 	 * @return
 	 */
 	public List<ChurchMember> findByAvailableWeekDay(int weekDay){
-		return null;
+		
+		StringBuilder sql = new StringBuilder();
+		
+	    sql.append("SELECT c FROM ChurchMember c ");
+		sql.append(" INNER JOIN c.family f ");
+		sql.append(" INNER JOIN f.familyAvailableWeekdays fawd");
+		sql.append(" WHERE ");
+		
+		if (Integer.parseInt(DaysOfTheWeekEnum.MONDAY.getId()) == weekDay) {
+			sql.append(" fawd.monday = ");
+		} else if ((Integer.parseInt(DaysOfTheWeekEnum.TUESDAY.getId()) == weekDay)){
+			sql.append(" fawd.tuesday = ");
+		} else if ((Integer.parseInt(DaysOfTheWeekEnum.WEDNESDAY.getId()) == weekDay)){
+			sql.append(" fawd.wednesday = ");
+		} else if ((Integer.parseInt(DaysOfTheWeekEnum.THURSDAY.getId()) == weekDay)){
+			sql.append(" fawd.thursday = ");
+		} else if ((Integer.parseInt(DaysOfTheWeekEnum.FRIDAY.getId()) == weekDay)){
+			sql.append(" fawd.friday = ");
+		} else if ((Integer.parseInt(DaysOfTheWeekEnum.SATURDAY.getId()) == weekDay)){
+			sql.append(" fawd.saturday = ");
+		} else if ((Integer.parseInt(DaysOfTheWeekEnum.SUNDAY.getId()) == weekDay)){
+			sql.append(" fawd.sunday = ");
+		}
+		
+		sql.append(" :param ");
+		
+		TypedQuery<ChurchMember> query = this.entityManager.createQuery(sql.toString(), ChurchMember.class);
+		
+		query.setParameter("param", 1);
+		
+		return query.getResultList();
 	}
 }
